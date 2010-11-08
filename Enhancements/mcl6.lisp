6279ec31d6dba76f2d536a51311e65fe450b3efe
Terje Norderhaug <terje@in-progress.com>
2010-11-07 16:47:33 -0800

    MCL6 build script

---
+++ Enhancements/mcl6.lisp
@@ -0,0 +1,3 @@
+(in-package :ccl)(dolist (file (directory (merge-pathnames "*.lisp" (loading-file-source-file))))  (unless (equal (full-pathname file)                 (full-pathname (loading-file-source-file)))    (load file))); #p"home:Level-0;l0-init.lisp"(add-feature :mcl-6.0); #p"home:Level-1;l1-boot-1.lisp"(defun lisp-implementation-version ()
+  "Version 6.0")(provide :mcl6)
+
