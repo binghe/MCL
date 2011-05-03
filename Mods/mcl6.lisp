cd13b23f0a8352f33f935e2170b770cf37e1cc8f
Terje Norderhaug <terje@in-progress.com>
2011-05-03 15:03:58 -0700

    MCL6 includes backtrace fatality patch

--- Mods/mcl6.lisp
+++ Mods/mcl6.lisp
@@ -1,4 +1,4 @@
-;; Build MCL 6(in-package :ccl)(dolist (file (directory (merge-pathnames "*.lisp" (loading-file-source-file))))  (unless (equal (full-pathname file)                 (full-pathname (loading-file-source-file)))    (load file)))(require :fred-package-indicator "ccl:Library;fred-package-indicator"); #p"home:Level-0;l0-init.lisp"(add-feature :mcl-6.0); #p"home:Level-1;l1-boot-1.lisp"(let ((*warn-if-redefine* nil)
+;; Build MCL 6(in-package :ccl)(load "ccl:Patches;backtrace-fatality-patch.lisp")(dolist (file (directory (merge-pathnames "*.lisp" (loading-file-source-file))))  (unless (equal (full-pathname file)                 (full-pathname (loading-file-source-file)))    (load file)))(require :fred-package-indicator "ccl:Library;fred-package-indicator"); #p"home:Level-0;l0-init.lisp"(add-feature :mcl-6.0); #p"home:Level-1;l1-boot-1.lisp"(let ((*warn-if-redefine* nil)
       (*warn-if-redefine-kernel* NIL))
 (defun lisp-implementation-version ()
   "Version 6.0")) ; end redefine(provide :mcl6)
